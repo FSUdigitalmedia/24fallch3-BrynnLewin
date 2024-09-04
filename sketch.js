2939957d91624d46df370f23eef9aa19f146f73f
Brynn Lewin <bhl21@fsu.edu>
2024-09-04 15:03:21 -0400

    not working, only one circle

--- sketch.js
+++ sketch.js
@@ -1,11 +1,11 @@
 let x1 = 100;     // x position of the ball
-let y1 = 0;       // y position of the ball
+let y1 = 60;       // y position of the ball
 let x1Speed = 3;  // speed in X direction
 let y1Speed = 7; // speed in Y direction
 let d1 = 20;      // diameter of the ball
 //second shape
-let x2 = 100;     // x position of the ball
-let y2 = 0;       // y position of the ball
+let x2 = 50;     // x position of the ball
+let y2 = 90;       // y position of the ball
 let x2Speed = 3;  // speed in X direction
 let y2Speed = 7; // speed in Y direction
 let d2 = 20;      // diameter of the ball
@@ -32,3 +32,21 @@ function draw() {
 
   ellipse(x1, y1, d1);
 }
+
+// second shape 
+background(220); // try commenting this out!
+ 
+// if x hits the right side or the left side of the canvas,
+// switch directions...
+if (x2 > width || x2 < 0) {
+  x2Speed = x2Speed * -1;  
+}
+x2 = x2 + x2Speed; // adding a negative xSpeed *decreases* x, right?
+
+// same as above, but for the top and bottom of the canvas
+if (y2 > width || y2 < 0) {
+  y2Speed = y2Speed * -1;  
+}
+y2 = y2 + y2Speed;
+
+circle(y1, y2, d2);
