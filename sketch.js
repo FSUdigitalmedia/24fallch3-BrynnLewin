d4f05b75058c2090d48022f3ca6e824abe05d9fb
Brynn Lewin <bhl21@fsu.edu>
2024-09-05 11:47:52 -0400

    random circle & rect

--- sketch.js
+++ sketch.js
@@ -15,38 +15,38 @@ function setup() {
 }
 
 function draw() {
-  background(220); // try commenting this out!
- 
+  background(244,25); // try commenting this out!
+
   // if x hits the right side or the left side of the canvas,
   // switch directions...
   if (x1 > width || x1 < 0) {
-    x1Speed = x1Speed * -1;  
+    x1Speed = x1Speed * -1;
   }
   x1 = x1 + x1Speed; // adding a negative xSpeed *decreases* x, right?
-  
+
   // same as above, but for the top and bottom of the canvas
   if (y1 > width || y1 < 0) {
-    y1Speed = y1Speed * -1;  
+    y1Speed = y1Speed * -1;
   }
   y1 = y1 + y1Speed;
+  fill(random(255),random(255),random(255))
+  circle(x1, y1, d1);
+  // second shape 
+  
+  // if x hits the right side or the left side of the canvas,
+  // switch directions...
+  if (x2 > width || x2 < 0) {
+    x2Speed = x2Speed * -1;
+  }
+  x2 = x2 + x2Speed; // adding a negative xSpeed *decreases* x, right?
 
-  ellipse(x1, y1, d1);
+  // same as above, but for the top and bottom of the canvas
+  if (y2 > width || y2 < 0) {
+    y2Speed = y2Speed * -1;
+  }
+  y2 = y2 + y2Speed;
+
+  rect(y1, y2, d2);
 }
 
-// second shape 
-background(220); // try commenting this out!
- 
-// if x hits the right side or the left side of the canvas,
-// switch directions...
-if (x2 > width || x2 < 0) {
-  x2Speed = x2Speed * -1;  
-}
-x2 = x2 + x2Speed; // adding a negative xSpeed *decreases* x, right?
 
-// same as above, but for the top and bottom of the canvas
-if (y2 > width || y2 < 0) {
-  y2Speed = y2Speed * -1;  
-}
-y2 = y2 + y2Speed;
-
-circle(y1, y2, d2);
